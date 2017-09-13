0a962f3f801d654c8a8f38329d8052882cb6d118
Svarok <dima@flex.by>
2017-09-13 10:24:47 +0300

    extinguished fires

--- bitrix/templates/main/_scripts/send_mail.php
+++ bitrix/templates/main/_scripts/send_mail.php
@@ -2,4 +2,4 @@
 
 require "SendMail.php";
 
-new \FlexMedia\SendMail($_REQUEST, array("send_to" => "info@gildia-sommelier.com", "subject" => "Заполнена форма на сайте"));
+new \FlexMedia\SendMail($_REQUEST, array("send_to" => "dima@flex.by", "subject" => "Заполнена форма на сайте"));
